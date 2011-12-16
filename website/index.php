0ba650ed8fd35f385366df369fa24d7328ecb2c1
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-12-16 11:10:58 +0400

    remove the unused variable copypasted from examples cleanup comments

--- website/index.php
+++ website/index.php
@@ -1,9 +1,4 @@
 <?
-/*$content = "Index page contents";
-$title = "HSE Shop --- Home Page";
-include("views/page.php");
-*/
-
 require_once("smarty/libs/Smarty.class.php");
 $smarty = new Smarty();
 
@@ -12,9 +7,7 @@ $smarty->setCompileDir('cache/smarty_cache/templates_cache');
 $smarty->setCacheDir('smarty/smarty_cache/main_cache');
 $smarty->setConfigDir('smarty/configs');
 
-$smarty->assign('name', 'Ned');
 $smarty->display('header.tpl');
 $smarty->display('index.tpl');
 $smarty->display('footer.tpl');
-
 ?>
