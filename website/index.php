3489e849b953d6316d3b0c3ba3dc803e69401360
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-12-16 09:30:17 +0400

    Add css and page template

---
+++ website/index.php
@@ -0,0 +1,5 @@
+<?
+$content = "Index page contents";
+$title = "HSE Shop --- Home Page";
+include("views/page.php");
+?>
---
+++ website/views/page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<link rel="stylesheet" href="css/styles.css"/>
+<title><? echo $title ?></title>
+</head>
+
+<body>
+	<?include("views/menu.php");?>
+	<div id="content">
+		<? echo $content ?>
+	</div>
+	<div id="footer">
+		<p>foo</p>
+	</div>
+</body>
+</html>
